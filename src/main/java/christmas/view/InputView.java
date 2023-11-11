dcaff4ac5a5473bdbd8b216231ffd3acd160c492
gyu_hyeok <jjookki33@gmail.com>
2023-11-12 00:28:09 +0900

    feat/입력 기능 리팩토링

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,13 +3,16 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String visitDay = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String orderMenu = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
     public String inputVisitDay() {
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        System.out.println(visitDay);
         return Console.readLine();
     }
 
     public String inputOrderMenu() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        System.out.println(orderMenu);
         return Console.readLine();
     }
 }
