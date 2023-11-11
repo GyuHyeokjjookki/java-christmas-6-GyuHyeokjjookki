22767b7f2929e7440efe0df6aea1c03c8f555955
gyu_hyeok <jjookki33@gmail.com>
2023-11-12 00:20:49 +0900

    feat/상수 변수 구현

---
+++ src/main/java/christmas/consts/ConstValue.java
@@ -0,0 +1,27 @@
+package christmas.consts;
+
+public class ConstValue {
+    public static final String MAIN = "메인";
+    public static final String DESSERT = "디저트";
+    public static final String DRINK = "음료";
+    public static final String NONE = "없음";
+    public static final String CAMPAGNE = "샴페인";
+
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int FIRST_DAY = 1;
+    public static final int LAST_DAY = 31;
+    public static final int CHRISTMAS_DAY = 25;
+    public static final int FIRSTDAY_CHRISTMAS_SALES = 1000;
+    public static final int SPECIAL_SALES = 1000;
+    public static final int SPLIT_INTEGER = 1000;
+    public static final int INCREASE_SALE_PRICE = 100;
+    public static final int PRESENT_BASE_PRICE = 120000;
+    public static final int WEEK_DAYS = 7;
+    public static final int CHECK_WEEKEND_FRI = 1;
+    public static final int CHECK_WEEKEND_SAT = 2;
+    public static final int MIN_BASE_SALE_PRICE = 10000;
+    public static final int MAX_ORDER_QUANTITY = 20;
+    public static final int WEEKEND_SALE_PRICE = 2023;
+    public static final int WEEKDAY_SALE_PRICE = 2023;
+}
