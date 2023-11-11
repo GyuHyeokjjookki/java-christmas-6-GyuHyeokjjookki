9a1b3b954fb3de844a5c9353e23fe94bc2e95091
gyu_hyeok <jjookki33@gmail.com>
2023-11-12 00:34:11 +0900

    feat/입력을 리스트로 변환 기능 구현

---
+++ src/main/java/christmas/service/ListConverter.java
@@ -0,0 +1,10 @@
+package christmas.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListConverter {
+    public List<String> convertToList(String[] args) {
+        return Arrays.asList(args);
+    }
+}
